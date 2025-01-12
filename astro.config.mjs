362af89437f089078c634b491544f19ceac6efb9
skv <xaku68@gmail.com>
2025-01-12 13:39:19 +0300

    Fix submodule issue

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import purgecss from 'astro-purgecss';
+
+export default defineConfig({
+	site: 'https://skvtmb.github.io/', // Укажите URL GitHub Pages
+	integrations: [mdx(), sitemap(), purgecss()],
+  });
---
+++ src/content/blog/first-post.md
@@ -0,0 +1,10 @@
+---
+title: 'First post'
+description: 'Мой первый пост'
+pubDate: 'Jan 11 2022'
+heroImage: '/First.jpg'
+---
+
+#### Мой первый пост
+
+*Тестирование движка Astro*
